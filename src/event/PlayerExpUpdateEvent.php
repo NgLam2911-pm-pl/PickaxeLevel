0d615d016655649572450458d60611c26cb413ae
NgLamIsSimp <nglamvn2911@gmail.com>
2021-09-24 21:10:43 +0700

    maybe it ok ?

--- src/event/PlayerExpUpdateEvent.php
+++ src/event/PlayerExpUpdateEvent.php
@@ -5,24 +5,37 @@ namespace PickaxeLevel\event;
 
 use PickaxeLevel\event\utils\Issuer;
 use PickaxeLevel\Loader;
+use PickaxeLevel\session\PlayerSession;
 use pocketmine\event\Cancellable;
 use pocketmine\event\CancellableTrait;
 use pocketmine\event\plugin\PluginEvent;
-use pocketmine\player\Player;
 use pocketmine\Server;
 
 class PlayerExpUpdateEvent extends PluginEvent implements Cancellable{
 	use CancellableTrait;
 
-	public function __construct(Player $player, Issuer $issuer){
+	protected PlayerSession $session;
+	protected Issuer $issuer;
+
+	public function __construct(PlayerSession $session, Issuer $issuer){
 		parent::__construct($this->getLoader());
+		$this->session = $session;
+		$this->issuer = $issuer;
 	}
 
-	public function getLoader() : ?Loader{
+	protected function getLoader() : ?Loader{
 		$loader = Server::getInstance()->getPluginManager()->getPlugin("PickaxeLevel");
 		if ($loader instanceof Loader){
 			return $loader;
 		}
 		return null;
+	}
+
+	public function getSession() : PlayerSession{
+		return $this->session;
+	}
+
+	public function getIssuer() : Issuer{
+		return $this->issuer;
 	}
 }
